364b53a4d86ba6025abc2e3d81ee37425fb2b3f8
salman3009 <salman444g@gmail.com>
2022-11-30 21:47:55 +0530

    code

---
+++ November/30-11-22/authentication-process-authorization/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const studentRouter = require('./routes/student');
+const userRouter = require('./routes/user');
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/tres").then((success)=>{
+     console.log("Database is connected successfully");
+}).catch((error)=>{
+      console.log("connection failed");
+});
+
+
+
+app.use('',userRouter);
+app.use('/student',studentRouter);
+
+
+app.listen(8080,()=>{
+    console.log("server is runing on 8080");
+});
